c6bba5e337b1b420c966d4bd44c7626d9fd3c795
Alexander Wolff <wolffaxn@gmail.com>
2020-10-28 19:21:16 +0100

    better error handling

--- example/main.rs
+++ example/main.rs
@@ -1,8 +1,14 @@
+use std::io::Error;
+use std::io::ErrorKind;
+
 fn echo(args: &[&str]) {
-    println!("{}", args.join(" "))
+    if args.len() < 1 {
+        Error::new(ErrorKind::InvalidInput, "Too few arguments");
+    }
+    println!("{}", args.join(" "));
 }
 
 fn main() {
     let args = ["hello,", "world"];
-    echo(&args)
+    echo(&args);
 }
